c1ebd393a735123bb9139e2459d7b204985087fd
that0n3guy <that0n3guy@46440.no-reply.drupal.org>
2014-08-18 16:51:13 -0500

    changed webserver user and fixed type on nginx.conf

--- create_site_simple.sh
+++ create_site_simple.sh
@@ -74,7 +74,7 @@ sudo $SED -i "s!ROOT!$WEB_DIR!g" $CONFIG
 
 # set up web root
 #sudo mkdir $WEB_DIR/$SITE_DIR
-sudo chown nginx:nginx -R $WEB_DIR
+sudo chown www:www -R $WEB_DIR
 sudo chmod 600 $CONFIG
 
 # create symlink to enable site
